dbeb0ea2d37cb9fb08ab68bb2533a503be447bce
Johnathan <johnathan@mistyrobotics.com>
2020-01-29 09:41:41 -0700

    Add licensing to skill code files

--- conciergeBaseTemplate/conciergeBaseTemplate.js
+++ conciergeBaseTemplate/conciergeBaseTemplate.js
@@ -1,3 +1,20 @@
+/**********************************************************************
+Copyright 2020 Misty Robotics, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+imitations under the License.
+**********************************************************************/
+
+
 // The skill is not set to start on boot up. To change this add "Startup" to "StartupRules" arrray in the JSON file.
 
 // ============================= Set Your Credentials Here ================================
